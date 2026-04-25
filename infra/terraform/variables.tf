a8b5d70b1b15ec16c4264299129d55310eecc7a3
Sagar-7845 <sagar.govalkar23@spit.ac.in>
2026-04-25 05:49:59 +0000

    feat: Phase 3 - restore Terraform files

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" { default = "ap-south-1" }
+variable "instance_type" { default = "t3.micro" }
+variable "ami_id" { default = "ami-0f58b397bc5c1f2e8" }
+variable "key_name" { default = "ecommerce-key" }
+variable "app_name" { default = "devops-ecommerce" }
