71009f65ce5699c86f295bf7f29bb307e156df9a
Sagar-7845 <sagar.govalkar23@spit.ac.in>
2026-04-26 11:29:19 +0000

    fix: update seed.py to use docker network mongodb host

--- backend/seed.py
+++ backend/seed.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import json
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb://mongodb:27017/")
 db = client["ecommerce"]
 db.products.drop()
 
